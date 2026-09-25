694ce40e4b1dc6520b45b6ed246282a46c4abf55
deliveryalt <106606047+phuck-bich@users.noreply.github.com>
2026-09-25 11:24:51 +0400

    Avoid duplicate BlockHighlight rendering

--- src/main/java/me/alpha432/oyvey/features/modules/render/BlockHighlight.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/BlockHighlight.java
@@ -1,6 +1,5 @@
 package me.alpha432.oyvey.features.modules.render;
 
-import com.google.common.eventbus.Subscribe;
 import me.alpha432.oyvey.event.impl.Render3DEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.util.render.RenderUtil;
@@ -12,10 +11,10 @@ import java.awt.*;
 
 public class BlockHighlight extends Module {
     public BlockHighlight() {
-        super("BlockHighlight", "Draws box at the block that you are looking at", Category.RENDER, true, false, false);
+        super("BlockHighlight", "Draws box at the block that you are looking at", Category.RENDER, false, false, false);
     }
 
-    @Subscribe public void onRender3D(Render3DEvent event) {
+    public void onRender3D(Render3DEvent event) {
         if (mc.crosshairTarget instanceof BlockHitResult result) {
             VoxelShape shape = mc.world.getBlockState(result.getBlockPos()).getOutlineShape(mc.world, result.getBlockPos());
             if (shape.isEmpty()) return;
